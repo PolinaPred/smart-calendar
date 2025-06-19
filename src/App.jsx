4b865cbb06173e49789a76cc8d7ac230e8fd4a80
MiniTigra <minitigrayt@gmail.com>
2025-06-19 16:54:38 -0400

    Tasks save in local storage to persist after browser refresh/closing
    Saves tasks to local storage so you can revisit your calendar on your device

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import TaskForm from './components/TaskForm';
 import viteLogo from '/vite.svg';
@@ -6,9 +6,28 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("smart-tasks");
+    if (saved) {
+      setTasks(JSON.parse(saved));
+    }
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if(loaded){
+      localStorage.setItem("smart-tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
 
   const addTask = (task) => {
     setTasks(prev => [...prev, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(prev => prev.filter(task => task.id !== id));
   };
 
   return (
@@ -20,6 +39,9 @@ function App() {
         {tasks.map(t => (
           <li key={t.id}>
             {t.title} ({t.locked ? "Stone" : "Sand"}, {t.duration} hr{t.duration !== 1 ? 's' : ''})
+            <button onClick={() => deleteTask(t.id)} style = {{ marginLeft: '1rem'}}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
