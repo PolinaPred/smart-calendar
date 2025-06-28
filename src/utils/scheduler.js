2f69d2890c405a4f3b93278094630f1f466acf59
MiniTigra <minitigrayt@gmail.com>
2025-06-28 08:05:27 -0400

    Was trying to prevent the same type of task from bunching together and instead have variety to avoid burnout, but this doesn't seem to work the way I wanted, so will probably need a different approach.

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -285,7 +285,7 @@ export function generateWeekSchedule(tasks) {
     const grid = initWeekGrid();
 
     const expandedTasks = expandRepeatingTasks(tasks);
-    const groupedByOriginal = {};
+    const unlockedClones = [];
     for (let task of expandedTasks){
         if(task.locked && task.scheduledAt){
             const date = new Date(task.scheduledAt);
@@ -305,21 +305,14 @@ export function generateWeekSchedule(tasks) {
                 bufferBefore,
                 bufferAfter
             });
-        }
-
-        if(!task.locked){
-            const key = task.originalId || task.id;
-            if (!groupedByOriginal[key]) groupedByOriginal[key] = [];
-            groupedByOriginal[key].push(task);
+        } else {
+            unlockedClones.push(task);
         }
     }
 
-    const allGroups = Object.values(groupedByOriginal);
+    //const allGroups = Object.values(groupedByOriginal);
 
-    allGroups.sort((groupA, groupB) => {
-        const a = groupA[0];
-        const b = groupB[0];
-
+    unlockedClones.sort((a, b) => {
         const aStrict = a.strictStart || a.strictEnd ? 1 : 0;
         const bStrict = b.strictStart || b.strictEnd ? 1 : 0;
 
@@ -332,30 +325,55 @@ export function generateWeekSchedule(tasks) {
         return bScore - aScore;
     });
 
-    for (const group of allGroups){
+    const typeBuckets = {};
+    for (let task of unlockedClones){
+        const key = task.originalId || task.id;
+        if (!typeBuckets[key]) typeBuckets[key] = [];
+        typeBuckets[key].push(task);
+    }
 
+    const interleaved = [];
+    let added;
+    do {
+        added = false;
+        for (let key of Object.keys(typeBuckets)){
+            const list = typeBuckets[key];
+            if(list.length > 0){
+                const i = Math.floor(Math.random() * list.length);
+                interleaved.push(list.splice(i, 1)[0]);
+                added = true;
+            }
+        }
+    } while (added);
+
+    const scheduledCount = {};
+
+    for (const clone of interleaved){
+        const key = getTaskKey(clone);
+        const maxPer = clone.maxPer;
+        const count = scheduledCount[key] || 0;
+
+        /*
         const baseTask = group[0];
         const maxPerValue = baseTask.maxPer?.value || null;
         const maxPerUnit = baseTask.maxPer?.unit || null;
         
         const internalOrder = [...group].sort(() => Math.random() - 0.5);
-        
-        if(maxPerUnit === 'week'){
-            let inserted = 0;
-            for (const clone of internalOrder){
-            if(inserted >= maxPerValue) break;
+        */
+       
+        if(clone.maxPer?.unit === 'week' && count >= clone.maxPer.value) continue;
+
             const beforeInsert = JSON.stringify(grid);
             insertFlexible(clone, grid);
             const afterInsert = JSON.stringify(grid);
             if(beforeInsert !== afterInsert){
-                inserted++;
+                scheduledCount[key] = (scheduledCount[key] || 0) +1;
             }
-            }
-        } else {
+        /*} else {
             for (const clone of internalOrder){
                 insertFlexible(clone, grid);
             }
-        }
+        }*/
     }
 
     for (const day of DAYS){
